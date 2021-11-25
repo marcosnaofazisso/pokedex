26953800a11d3d612d0510cf0c314fb8548e06b4
Marcos Ferreira <marcosferreiraf22@gmail.com>
2021-11-25 20:54:12 -0300

    add form style and imgs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React from "react"
-import Home from "./pages/home/Home"
 import Header from "./pages/header/Header"
 import Footer from "./pages/footer/Footer"
 import Routes from "./Routes"
@@ -9,7 +8,6 @@ export default function App() {
     return (
         <>
             <Header />
-            <h1>Pokedex</h1>
             <Routes />
             <Footer />
         </>
--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import {Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Home from './pages/home/Home'
 import AllPokemons from './pages/allpokemons/AllPokemons'
 import Pokemon from './pages/pokemon/Pokemon';
 import Pokebag from './pages/pokebag/Pokebag';
 
 function Routes() {
-    return (
-      <>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/allpokemons" component={AllPokemons} />
-          <Route path="/pokemon/:id" component={Pokemon} />
-          <Route path="/pokebag" component={Pokebag} />
-        </Switch>
-      </>
-    );
-  }
-  
-  export default Routes;
+  return (
+    <>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/allpokemons" component={AllPokemons} />
+        <Route path="/pokemon/:id" component={Pokemon} />
+        <Route path="/pokebag" component={Pokebag} />
+      </Switch>
+    </>
+  );
+}
+
+export default Routes;
