df3fe59063ffcabd0c8f57f57faa5cca052a5c58
Marcos Ferreira <marcosferreiraf22@gmail.com>
2022-02-20 12:18:06 -0300

    created a styled for pokebag

--- frontend/src/pages/pokebag/Pokebag.jsx
+++ frontend/src/pages/pokebag/Pokebag.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { usePokebag } from '../../components/context/PokebagContextProvider';
 
+import { CardPokebag, CardButtonPokebag } from '../../assets/style/StyledPokebag';
+
 export default function Pokebag() {
 
     const { poke1, setPoke1,
@@ -12,26 +14,13 @@ export default function Pokebag() {
 
     return (
         <div>
-            <h1>Pokebag</h1>
-            <span><b>Poke 1: </b>{poke1}</span>
-            <button onClick={() => setPoke1("Hitmonlee")}>Set Poke1</button>
-            <br />
-            <span><b>Poke 2: </b>{poke2}</span>
-            <button onClick={() => setPoke2("Machamp")}>Set Poke2</button>
-            <br />
-            <span><b>Poke 3: </b>{poke3}</span>
-            <button onClick={() => setPoke3("Butterfree")}>Set Poke3</button>
-            <br />
-            <span><b>Poke 4: </b>{poke4}</span>
-            <button onClick={() => setPoke4("Pikachu")}>Set Poke4</button>
-            <br />
-            <span><b>Poke 5: </b>{poke5}</span>
-            <button onClick={() => setPoke5("Charmander")}>Set Poke5</button>
-            <br />
-            <span><b>Poke 6: </b>{poke6}</span>
-            <button onClick={() => setPoke6("Squirtle")}>Set Poke6</button>
-            <br />
-        </div>
+            <CardPokebag>
+                <p>#</p>
+                <img src="" />
+                <h3></h3>
+                <span></span>
+            </CardPokebag>
+        </div >
     )
 }
 
